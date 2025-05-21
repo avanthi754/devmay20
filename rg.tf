baf7a30d30787393ac774cde030785170f84ba64
avanthi754 <78343982+avanthi754@users.noreply.github.com>
2025-05-21 08:20:45 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,9 +1,6 @@
 resource "azurerm_resource_group" "name" {
     name = "RG-01"
     location = "EastUS"
-<<<<<<< HEAD
-    location = "centralUS"
-=======
+    location = "centralUS
     location = "westus"
->>>>>>> 2f0689ba1dd068eab6e3ae8486292d4cdae3aa1e
 }
